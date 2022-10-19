607a606be6b20bdde2639ab9b67dc1d9e51cba6e
yuta051214 <ffftrick123@gmail.com>
2022-10-19 15:37:16 +0900

    Initial Commit

---
+++ TableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewApp
+//
+//  Created by kimura yuta on 2022/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
